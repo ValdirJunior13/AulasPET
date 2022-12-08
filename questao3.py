6e8ac5bd7040b3027eb214feffa6492c8ed8e12e
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2022-12-08 15:31:10 +0000

    Mudança Pequena

--- questao3.py
+++ questao3.py
@@ -1,6 +1,6 @@
 valor = int(input("Digite sua senha: "))
 while valor != "2002":
-  valor = input("Senha incorreta!! \nPor favor, digitar novamente: ")
+  valor = int(input("Senha incorreta!! \nPor favor, digitar novamente: "))
 
 else:
   print("Senha correta! \nAcesso autorizado!!")
