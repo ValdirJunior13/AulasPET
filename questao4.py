c03b71c7b51df40172d3b6790b638cf295bdfdf0
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2022-12-08 02:05:38 +0000

    mudança

--- questao4.py
+++ questao4.py
@@ -1,14 +1,15 @@
 idade = []
+valor = int(input("Digite uma idade: "))
 i = 0
-
-while i>=0:
+while valor >= 0:
   valor = int(input("Digite uma idade: "))
   idade.append(valor)
-  i = i + 1 
-  if idade[-1] < 0:
-      del idade[-1]
-      media = sum(idade) // len(idade)
-      print("Esses foram as idades digitadas {}, a média entre as idades {} e a quantidade de idades inseridas {}" .format((idade), media, i))
-      break    
+  i += 1
+
+else:
+  del idade[-1]
+  media = sum(idade) / len(idade)
+  print("Esses foram as idades digitadas {}, a média entre as idades é {} e a quantidade de idades inseridas foram {}" .format((idade), media, i))
+        
     
     
