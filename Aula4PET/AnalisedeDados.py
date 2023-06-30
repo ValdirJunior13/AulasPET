d3ceebcffcf8d04accb4774016c921887b4bef2e
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2023-06-30 15:28:44 +0000

    mudanças

--- Aula4PET/AnalisedeDados.py
+++ Aula4PET/AnalisedeDados.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from scipy import stats
 # importar os dados
-data2022 = pd.read_csv('licitacoes-2022.csv', sep=',',on_bad_lines='skip', low_memory=False)
-data2021 = pd.read_csv('licitacoes-2021.csv', sep=',',on_bad_lines='skip', low_memory=False)
+data2022 = pd.read_csv('', sep=',',on_bad_lines='skip', low_memory=False)
+data2021 = pd.read_csv('', sep=',',on_bad_lines='skip', low_memory=False)
 
 # Esse sample é uma amostra em que irei usar apenas os dados dos generos alimenticios
-sample1 = data2022[data2022['DESCRICAOOBJETO'] == 'GÊNEROS ALIMENTÍCIOS']['VALORESTIMADO'] 
-sample2 = data2021[data2021['DESCRICAOOBJETO'] == 'GÊNEROS ALIMENTÍCIOS']['VALORESTIMADO']
+sample1 = data2022[data2022['DESCRICAOOBJETO'] == '']['VALORESTIMADO'] 
+sample2 = data2021[data2021['DESCRICAOOBJETO'] == '']['VALORESTIMADO']
 sample1 = pd.to_numeric(sample1, errors='coerce')
 sample2 = pd.to_numeric(sample2, errors='coerce')
 
@@ -31,29 +31,29 @@ desvio2021 = sample2.std()
 
 
 # exibir o resultado
-print('Valor mediana 2022 %s' % mediana2022)
+print("Valor mediana 2022", mediana2022)
 print('-------------------------')
-print("Valor mediana 2021 %s" % mediana2021)
+print("Valor mediana 2021", mediana2021)
 print('-------------------------')
-print("Valor média 2022 %s" % media2022)
+print("Valor média 2022", media2022)
 print('-------------------------')
-print("Valor média 2021 %s" % media2021)
+print("Valor média 2021", media2021)
 print('-------------------------')
-print("Valor mínimo 2022 %s" % minimo2022)
+print("Valor mínimo 2022", minimo2022)
 print('-------------------------')
-print("Valor mínimo 2021 %s" % minimo2021)
+print("Valor mínimo 2021", minimo2021)
 print('-------------------------')
-print("Valor máximo 2022 %s" % maximo2022)
+print("Valor máximo 2022",maximo2022)
 print('-------------------------')
-print("Valor máximo 2021 %s" % maximo2021)
+print("Valor máximo 2021", maximo2021)
 print('-------------------------')
-print("Valor variância 2022 %s" % variancia2022)
+print("Valor variância 2022", variancia2022)
 print('-------------------------')
-print("Valor variância 2021 %s" % variancia2021)
+print("Valor variância 2021",  variancia2021)
 print('-------------------------')
-print("Valor desvio padrão 2022 %s" % desvio2022)
+print("Valor desvio padrão 2022", desvio2022)
 print('-------------------------')
-print("Valor desvio padrão 2021 %s" % desvio2021)
+print("Valor desvio padrão 2021",  desvio2021)
 
 
 # # Perform t-test
